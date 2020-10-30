0a3da2ce30f5c7e1e64e092eae81aea54ae59de0
motokikasai <kmomoky@hotmail.com>
2020-10-30 14:53:55 +0100

    Add logic to form inputs with spread operator

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,27 +1,46 @@
 import { useQuery } from '@apollo/client';
+import { useState } from 'react';
 import { getAuthorsQuery } from '../queries/queries';
 
 const AddBook = () => {
   const { loading, error, data } = useQuery(getAuthorsQuery);
+  const [state, setState] = useState({ name: '', genre: '', authorId: '' });
 
   if (loading) return <option disabled>Loading Authors...</option>;
   if (error) return <p>Something went wrong!</p>;
 
+  const handleSubmitForm = e => {
+    e.preventDefault();
+
+    console.log(state);
+  };
+
   return (
-    <form id='add-book'>
+    <form id='add-book' onSubmit={handleSubmitForm}>
       <div className='field'>
         <label>Book name:</label>
-        <input type='text' />
+        <input
+          type='text'
+          // value={state.name}
+          onChange={e => setState({ ...state, name: e.target.value })}
+        />
       </div>
 
       <div className='field'>
         <label>Genre:</label>
-        <input type='text' />
+        <input
+          type='text'
+          // value={state.genre}
+          onChange={e => setState({ ...state, genre: e.target.value })}
+        />
       </div>
 
       <div className='field'>
         <label>Author:</label>
-        <select>
+        <select
+          // value={state.authorId}
+          onChange={e => setState({ ...state, authorId: e.target.value })}
+        >
           <option>Select author</option>
           {data.authors.map(author => {
             return (
